ddcc97e2c885ab088dd749edc4ae4a2a2466b96a
Alexander Wauck <alex@impulse101.org>
2014-03-31 21:53:15 -0500

    Enhanced configure script

--- README.md
+++ README.md
@@ -7,21 +7,18 @@ jimgload is a fairly simple library for loading images (currently PNG or JPEG) i
 * libpng 1.2 or 1.6
 * libjpeg-turbo 1.3
 * JNI
-* Python 2 (compile-time only)
+* Python 2.7 (compile-time only)
 
 # Building
 
 IF YOU ARE USING ARCH LINUX: Arch does not have a critical symlink in the same directory as `jni.h`
 (in `/usr/lib/jvm/java-7-openjdk/include` on my system).  Basically, `jni.h` references `jni_md.h`,
-but it does so with quotes instead of angle brackets, and the directory `jni_md.h` is in (`include/linux`)
+but it does so with quotes instead of angle brackets, and `jni_md.h` is in `include/linux`, which
 probably isn't in your include path anyway.  So, you need a symlink to `jni_md.h` in the same
 directory as `jni.h`.
 
-Before you do anything else, you need to set `$JAVA_HOME`.  On a Debian/Ubuntu-type system, this
-is typically `/usr/lib/jvm/default-java`.  `$JAVA_HOME/include` is where the build system expects
-to find `jni.h`.
-
-Run `./configure.py`.  This will set up `jni/GNUmakefile` correctly.
+Run `./configure.py --jdk=/path/to/java_home`.  This will set up `jni/GNUmakefile` correctly.
+Run `./configure.py --help` for the full set of options.
 
 Run `ant -p` to see the list of targets.  Currently, these are:
 
@@ -43,6 +40,14 @@ compiled test code is in `build/test`.
 
 When running, `libjimgload.so` needs to be on `java.library.path`.  The most straightforward
 way to do this is to run with `-Djava.library.path=$whatever`.
+
+If you want to install with `ant install`, use `-Djar.install.dir=$whatever`.  This option is
+probably most useful for distribution packagers.
+
+To deploy to a local Maven repository (useful for Clojure development), use `ant -Dremote.repo=$whatever deploy`.
+If you do this, you will need to copy `libjimgload.so` yourself.
+
+Any improvements to the build system are VERY welcome.  I'd like the whole thing to be very simple for the user.
 
 # Portability
 
--- configure.py
+++ configure.py
@@ -1,18 +1,55 @@
 #!/usr/bin/env python2
 
 import os.path as path
+import argparse
 
-LIBDIR='/usr/lib'
+parser = argparse.ArgumentParser(description='Configure jimgload for building')
+parser.add_argument('--prefix', help='set prefix for installation', default='/usr')
+parser.add_argument('--libdir', help='set directory for installation of libraries')
+parser.add_argument('--includedir', help='set directory for installation of headers')
+parser.add_argument('--jdk', help='set directory for JDK (i.e. $JAVA_HOME)', required=True)
+#parser.add_argument('--with-libpng', help='specify a specific version of libpng')
+parser.add_argument('--dep-libdir', help='set directory to search for dependency libraries')
+parser.add_argument('--dep-includedir', help='set directory to search for dependency headers')
 
-libpng = 'png'
+args = parser.parse_args()
 
-if path.exists(path.join(LIBDIR, 'libpng16.so')):
-	libpng = 'png16'
-elif path.exists(path.join(LIBDIR, 'libpng12.so')):
-	libpng = 'png12'
+LIBDIR = args.libdir
+if not LIBDIR:
+  LIBDIR = path.join(args.prefix, 'lib')
+
+INCLUDEDIR = args.includedir
+if not INCLUDEDIR:
+  INCLUDEDIR = path.join(args.prefix, 'include')
+
+DEP_LIBDIR = args.dep_libdir
+if not DEP_LIBDIR:
+  DEP_LIBDIR = path.join(args.prefix, 'lib')
+
+DEP_INCLUDEDIR = args.dep_includedir
+if not DEP_INCLUDEDIR:
+  DEP_INCLUDEDIR = path.join(args.prefix, 'include')
+
+libpng = 'png12'
+
+libpng_includedir = DEP_INCLUDEDIR
+
+if path.exists(path.join(DEP_LIBDIR, 'libpng16.so')):
+  libpng = 'png16'
+elif path.exists(path.join(DEP_LIBDIR, 'libpng12.so')):
+  libpng = 'png12'
+  libpng12_subdir = path.join(DEP_INCLUDEDIR, 'libpng12')
+  if path.exists(libpng12_subdir):
+    libpng_includedir = libpng12_subdir
 
 with open('jni/GNUmakefile.in', 'rb') as infile:
-	intext = infile.read()
+  intext = infile.read()
 
 with open('jni/GNUmakefile', 'wb') as outfile:
-	outfile.write(intext.format(libpng=libpng))
+  outfile.write(intext.format(libpng=libpng,
+                              includedir=INCLUDEDIR,
+                              libdir=LIBDIR,
+                              dep_includedir=DEP_INCLUDEDIR,
+                              dep_libdir=DEP_LIBDIR,
+                              libpng_includedir=libpng_includedir,
+                              jdk=args.jdk))
